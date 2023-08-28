19138dff2c2d57b4566f391caebf3bc3faa1076d
rasgrin <stefanrogic@protonmail.com>
2023-08-28 19:32:26 +0200

    added author info and created some mockup data

--- src/components/cards/postCard/PostCard.tsx
+++ src/components/cards/postCard/PostCard.tsx
@@ -4,22 +4,41 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 
-const PostCard = () => {
-  const [selected, setSelected] = useState();
-  const [likedPost, setLikedPost] = useState(false);
+type PostCardData = {
+  question: string;
+  answers: Array<string>;
+  hashtag: string;
+};
+
+const PostCard = ({ question, answers, hashtag }: PostCardData) => {
+  const [voted, setVoted] = useState<boolean>(false);
+  const [selected, setSelected] = useState<number | null>(null);
+  const [likedPost, setLikedPost] = useState<boolean>(false);
 
   return (
-    <div className="container border rounded-lg p-8 w-full md:w-5/6">
-      <h2 className="text-2xl">What do you think of this site?</h2>
+    <div className="container border rounded-lg p-8 w-full lg:w-5/6">
+      <div className="flex items-center">
+        <div className="h-10 w-10 bg-black rounded-full"></div>
+        <p className="ml-5">Anonimous</p>
+        <p className="ml-auto">10m ago</p>
+      </div>
+      <h2 className="text-2xl mt-14">{question}</h2>
       <div className="space-y-4 mt-8">
-        {["It's great!", "It not that great..."].map((o: string, i: number) => (
+        {answers.map((o: string, i: number) => (
           <div key={i} className={`flex justify-between w-full p-4 border rounded-lg cursor-pointer ${selected === i && "border-brightRed"}`} onClick={() => setSelected(i)}>
             <p>{o}</p>
             <p>{selected === i ? "100%" : "0%"}</p>
           </div>
         ))}
       </div>
-      <button className="mt-14 py-3 px-7 text-white bg-brightRed hover:bg-brightRedLight rounded-lg ">Submit</button>
+
+      {voted ? (
+        <p className="mt-4 ml-1 text-darkGrayishBlue">1 vote</p>
+      ) : (
+        <button className="mt-14 py-3 px-7 text-white bg-brightRed hover:bg-brightRedLight rounded-lg" onClick={() => setVoted(true)}>
+          Submit
+        </button>
+      )}
 
       <div className="flex mt-14">
         <button className={`cursor-pointer ${likedPost && "text-red-600"}`} onClick={() => setLikedPost((prev) => (prev = !prev))}>
@@ -28,7 +47,7 @@ const PostCard = () => {
         <button className="ml-4 cursor-pointer">
           <ChatBubbleOutlineIcon /> 10
         </button>
-        <button className="ml-auto cursor-pointer text-brightRed">#random</button>
+        <button className="ml-auto cursor-pointer text-brightRed">#{hashtag}</button>
       </div>
     </div>
   );
---
+++ src/data/polls.tsx
@@ -0,0 +1,4 @@
+export const polls = [
+  { question: "What do you think of this site?", answers: ["It's great!", "It not that great..."], hashtag: "random" },
+  { question: "Who is the best athlete ever?", answers: ["Some guy", "Some other guy", "This guy"], hashtag: "sports" },
+];
